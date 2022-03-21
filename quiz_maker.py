2eab86a8660fa377aeff0fdfba6427d2f5a5b278
Cesar <rodascesar03@yahoo.com>
2022-03-21 16:16:23 -0500

    You can make a simple quiz now

--- quiz_maker.py
+++ quiz_maker.py
@@ -3,6 +3,27 @@ import os
 def cls():
   os.system('cls' if os.name == 'nt' else 'clear')
 
+def edit_quiz(file):
+  done = False
+  while (done == False):
+    q = input("Enter a question. ")
+    file.write("Q " + q + "\n")
+    a = input("Enter the correct answer. ")
+    file.write("A " + a + "\n")
+    choice1 = input("Enter an incorrect choice. ")
+    file.write("C1 " + choice1 + "\n")
+    choice2 = input("Enter another incorrect choice. ")
+    file.write("C2 " + choice2 + "\n")
+    choice3 = input("Enter a third incorrect choice. ")
+    file.write("C3 " + choice3 + "\n")
+    dun = input("Are you done? ")
+    if(dun == "yes" or dun == "y"):
+      print("Done!")
+      print("goodbye.")
+      done = True
+    else:
+      cls()
+
 quiz = input("Are we creating a new quiz? (yes/no) ")
 
 if(quiz == "yes" or quiz == "y"):
@@ -11,9 +32,30 @@ if(quiz == "yes" or quiz == "y"):
   folder = os.path.isdir('./quizzes')
   if(folder == False):
     os.mkdir('./quizzes')
-  cls()
-  print("question one")
+  file = os.path.exists('./quizzes/' + name + ".txt")
+  if(file == True):
+    print("This quiz already exists. ")
+    print("goodbye.")
+  else:
+    print("Creating your quiz... ")
+    f = open("./quizzes/" + name + ".txt", "a")
+    edit_quiz(f)
+    f.close()
+  #cls()
+  #print("question one")
 
 if(quiz == "no" or quiz == "n"):
-  print("goodbye.")
-  exit()
+  add_question = input("Are we adding a question to this quiz? ")
+  if(add_question == "yes" or add_question == "y"):
+    quiz_name = input("What is the quizz's name? ")
+    if(quiz_name[-4] != "txt"):
+      quiz_name = quiz_name + ".txt"
+    file = os.path.exists('./quizzes/' + quiz_name)
+    if(file == True):
+      f = open('./quizzes/' + quiz_name, "a")
+      edit_quiz(f)
+      f.close()
+
+  if(add_question == "no" or add_question == "n"):
+    print("goodbye.")
+    exit()
